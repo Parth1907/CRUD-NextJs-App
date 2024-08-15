bb6cd9a96726ad51af2aa0a7e04caabcd64e9deb
Parth Kalra <93028052+Parth1907@users.noreply.github.com>
2024-08-15 22:27:09 +0530

    added logo link

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,7 +5,9 @@ import {Button} from "@material-tailwind/react";
 export default function Header() {
 	return (
 		<div className="bg-black p-4 my-4 flex justify-between items-center">
-			<h1 className="text-center font-bold text-2xl text-white">All Tasks</h1>
+			<Link href="/">
+			    <h1 className="text-center font-bold text-2xl text-white">All Tasks</h1>
+			</Link>
 			<Link href={"/create"}>
 				<Button color="white">Create Task</Button>
 			</Link>
